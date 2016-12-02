382c1b5107fcef37fa2ab65657dc9151daa76049
Khaled Halabieh <khaled-h@hotmail.it>
2016-12-02 17:20:48 +0100

    Create Fax.java

---
+++ Fax.java
@@ -0,0 +1,13 @@
+
+
+/**
+ * @author Khaled Halabieh
+ * @version 1.0.0
+ *
+ */
+public interface Fax {
+	
+	String faxsimulation ="Das Senden wird simuliert";
+
+	 public void senden (String sendeRef);
+}
